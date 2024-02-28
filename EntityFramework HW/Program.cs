9bee9d359160aeb00841811b3694df65ba94b010
EHOKEHTY <jenya2003a@gmail.com>
2024-02-28 20:35:30 +0200

    Добавьте файлы проекта.

---
+++ EntityFramework HW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFramework_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
